441b9ad9fd7745940fbaddf8ffa132e2d019524c
xerifeazeitona <juliano.amaral@gmail.com>
2021-02-21 15:35:33 -0500

    added exercise 19-2

---
+++ exercises/19_1_blog/users/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+
+def register(request):
+    """Register a new user."""
+    if request.method != 'POST':
+        form = UserCreationForm()
+    else:
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            # Login and redirect to home page
+            login(request, new_user)
+            return redirect('blogs:index')
+
+    # Display a blank or invalid form
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
